945bd2c2356539ee70d648771e4d1939195bc168
Sc1pex <scipex.dev@gmail.com>
2024-11-21 14:06:48 +0200

    web: add prettier with plugins

--- web/src/components/Navbar.svelte
+++ web/src/components/Navbar.svelte
@@ -2,19 +2,17 @@
 </script>
 
 <nav
-    class="flex justify-between items-center bg-dark-blue p-4 text-white font-semibold text-[22px] rounded-b-[25px] px-10 shadow-md shadow-black/50"
+  class="flex items-center justify-between rounded-b-[25px] bg-dark-blue p-4 px-10 text-[22px] font-semibold text-white shadow-md shadow-black/50"
 >
-    <div class="flex gap-4 items-center">
-        <img src="logo-white.svg" class="h-[60px] mr-[100px]" />
+  <div class="flex items-center gap-4">
+    <img src="logo-white.svg" class="mr-[100px] h-[60px]" />
 
-        <div>Recipes</div>
-        <div>Add recipe</div>
-    </div>
+    <div>Recipes</div>
+    <div>Add recipe</div>
+  </div>
 
-    <div class="flex gap-8 mr-8 items-center">
-        <div class="border-white border-2 rounded-[21px] px-5 py-1.5">
-            Login
-        </div>
-        <div>Register</div>
-    </div>
+  <div class="mr-8 flex items-center gap-8">
+    <div class="rounded-[21px] border-2 border-white px-5 py-1.5">Login</div>
+    <div>Register</div>
+  </div>
 </nav>
