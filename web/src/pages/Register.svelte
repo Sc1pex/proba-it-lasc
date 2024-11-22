e91a5032bff45659de1f273a3786b0bced738eb0
Sc1pex <scipex.dev@gmail.com>
2024-11-22 10:43:13 +0200

    web: register page layout

--- web/src/pages/Register.svelte
+++ web/src/pages/Register.svelte
@@ -1,4 +1,65 @@
 <script lang="ts">
 </script>
 
-<div>Register page</div>
+<div class="mt-[10vh] flex items-start justify-center">
+  <form
+    class="w-[38vw] rounded-[20px] bg-dark-blue px-24 pb-16 pt-12 text-white"
+  >
+    <p
+      class="font-inter text-balance text-center text-[32px] font-bold leading-10"
+    >
+      Hai, fă foamea cu noi!
+    </p>
+
+    <div class="mt-16 flex border-b px-2 pb-3">
+      <img src="person.svg" class="mr-3 w-[30px]" />
+      <input
+        type="email"
+        class="bg-dark-blue placeholder-white focus:outline-none"
+        placeholder="Full name"
+      />
+    </div>
+
+    <div class="mt-8 flex border-b px-2 pb-3">
+      <img src="phone.svg" class="mr-3 h-[30px] w-[30px]" />
+      <input
+        type="email"
+        class="bg-dark-blue placeholder-white focus:outline-none"
+        placeholder="Telephone"
+      />
+    </div>
+
+    <div class="mt-8 flex border-b px-2 pb-3">
+      <img src="mail.svg" class="mr-3 w-[30px]" />
+      <input
+        type="email"
+        class="bg-dark-blue placeholder-white focus:outline-none"
+        placeholder="E-mail"
+      />
+    </div>
+
+    <div class="mt-8 flex border-b px-2 pb-3">
+      <img src="lock.svg" class="mr-3 w-[30px]" />
+      <input
+        type="password"
+        class="bg-dark-blue placeholder-white focus:outline-none"
+        placeholder="Password"
+      />
+    </div>
+
+    <div class="mt-8 flex border-b px-2 pb-3">
+      <img src="lock.svg" class="mr-3 w-[30px]" />
+      <input
+        type="password"
+        class="bg-dark-blue placeholder-white focus:outline-none"
+        placeholder="Confirm Password"
+      />
+    </div>
+
+    <div class="mt-16 text-center">
+      <button class="bg-green rounded-lg px-24 py-2 text-[32px] font-bold">
+        Sign up
+      </button>
+    </div>
+  </form>
+</div>
