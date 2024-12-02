7e331bd5f47c56c0a3ecc799c8a59f0a7568be25
Sc1pex <scipex.dev@gmail.com>
2024-12-02 23:45:28 +0200

    web: add box shadow to recipes

--- web/src/components/Recipe.tsx
+++ web/src/components/Recipe.tsx
@@ -16,12 +16,13 @@ export function RecipeComponent({
   const border = vertical
     ? "border-t-green border-t-2"
     : "border-l-green border-l-2";
+  const flex = vertical ? "flex-col flex" : "flex";
 
   const img = img_url || "tmp/pizza.png";
 
   return (
     <div
-      className={vertical ? "flex-col flex bg-white" : "flex bg-white"}
+      className={flex + " bg-white shadow-[1px_1px_15px_-3px_rgba(0,0,0,0.25)]"}
       onClick={on_click}
     >
       <img src={img} className="w-[14vw] aspect-square object-contain" />
